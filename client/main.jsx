e661c8b220879cc4d870a42393fc259ab9011fbe
Yen Yin Hing <yenyinf287@gmail.com>
2019-06-18 20:25:31 -0700

    import accountsconfig file

--- client/main.jsx
+++ client/main.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
-import App from '/imports/ui/App'
+import App from '/imports/ui/App';
+import '../imports/startup/accountconfig';
+
 
 Meteor.startup(() => {
   render(<App />, document.getElementById('react-target'));

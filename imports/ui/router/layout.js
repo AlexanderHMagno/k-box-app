e8bc2feb3b04eba396a4ca1d87f35a8585e9127a
Yen Yin Hing <yenyinf287@gmail.com>
2019-06-21 17:05:06 -0700

    Incorporate favorite component functionality

--- imports/ui/router/layout.js
+++ imports/ui/router/layout.js
@@ -1,17 +1,40 @@
-import React, { Fragment } from "react";
+import React, { Fragment, Component } from "react";
 import { Redirect, Route, Switch } from "react-router";
 import Home from "../pages/Home";
 import Profile from "../pages/Profile";
 import Karoke from "../pages/Karoke";
+import UserFavorites from "../components/siteLayout/UserFavorites";
 
-export default () => (
-  <Fragment>
-    <Switch>
-      <Route exact path="/home" component={Home} />
-      <Route exact path="/karoke" component={Karoke} />
-      <Route exact path="/profile" component={Profile} />
-      <Route exact path="/profile/:userid" component={Profile} />
-      <Redirect from="*" to="/karoke" />
-    </Switch>
-  </Fragment>
-);
+class Router extends Component {
+  render() {
+    if (Meteor.user()) {
+      return (
+        <Fragment>
+          <Switch>
+            <Route exact path="/karoke" component={Karoke} />
+            <Route exact path="/profile" component={Profile} />
+            <Route exact path="/profile/:userid" component={Profile} />
+            <Route exact path="/favorites" component={UserFavorites} />
+            <Redirect from="*" to="/profile" />
+          </Switch>
+        </Fragment>
+      );
+    } else {
+      return (
+        <Fragment>
+          <Switch>
+            <Route exact path="/home" component={Home} />
+            <Redirect from="*" to="/home" />
+            {/* 
+            <Route exact path="/karoke" component={Karoke} />
+            <Route exact path="/profile" component={Profile} />
+            <Route exact path="/profile/:userid" component={Profile} />
+            <Route exact path="/favorites" component={UserFavorites} />
+            <Redirect from="*" to="/profile" /> */}
+          </Switch>
+        </Fragment>
+      );
+    }
+  }
+}
+export default Router;

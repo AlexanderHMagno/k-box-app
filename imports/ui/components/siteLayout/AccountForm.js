bb8517e79dcaa6906e0b17dfdd94755f10f55c80
Yen Yin Hing <yenyinf287@gmail.com>
2019-06-30 10:04:43 -0700

    add validate feature

--- imports/ui/components/siteLayout/AccountForm.js
+++ imports/ui/components/siteLayout/AccountForm.js
@@ -11,8 +11,9 @@ import PropTypes from "prop-types";
 import styles from "../styles";
 import { Meteor } from "meteor/meteor";
 import { Links } from "../../../api/links";
-
+import validate from "../helpers/validation";
 import { withRouter } from "react-router-dom";
+import { FORM_ERROR } from "final-form";
 
 class AccountForm extends Component {
   constructor(props) {
@@ -28,17 +29,25 @@ class AccountForm extends Component {
     return (
       <Form
         onSubmit={values => {
-          if (this.state.formToggle) {
-            Meteor.loginWithPassword(values.email, values.password, er => {
-              if (er) {
-                throw new Meteor.Error("Inccorrect Email or Password");
-              }
-            });
-          } else {
-            Accounts.createUser(values, er => {
-              if (er) {
-                throw new Meteor.Error("Existing Account already exists");
-              } else {
+          return new Promise((resolve, reject) => {
+            if (this.state.formToggle) {
+              Meteor.loginWithPassword(values.email, values.password, er => {
+                if (er) {
+                  resolve({
+                    [FORM_ERROR]: "Incorrect Email or Password"
+                  });
+                  return;
+                }
+                resolve();
+              });
+            } else {
+              Accounts.createUser(values, er => {
+                if (er) {
+                  resolve({
+                    [FORM_ERROR]: "Existing Account already exists"
+                  });
+                  return;
+                }
                 //Creates de user account
                 Links.insert({
                   _id: Meteor.userId(),
@@ -50,10 +59,13 @@ class AccountForm extends Component {
                   friendRequests: [],
                   rooms: []
                 });
-              }
-            });
-          }
+                resolve();
+                return;
+              });
+            }
+          });
         }}
+        validate={validate}
         render={({
           handleSubmit,
           pristine,
@@ -62,104 +74,117 @@ class AccountForm extends Component {
           submitting,
           submitError,
           hasSubmitErrors
-        }) => (
-          <form onSubmit={handleSubmit}>
-            {!this.state.formToggle && (
-              <FormControl fullWidth className={classes.formControl}>
-                <InputLabel htmlFor="fullname">Username</InputLabel>
+        }) => {
+          return (
+            <form onSubmit={handleSubmit}>
+              {!this.state.formToggle && (
+                <FormControl fullWidth className={classes.formControl}>
+                  <InputLabel htmlFor="username">Username</InputLabel>
 
+                  <Field
+                    name="username"
+                    render={({ input, meta }) => (
+                      <>
+                        <Input
+                          {...input}
+                          id="username"
+                          type="text"
+                          inputProps={{
+                            autoComplete: "off"
+                          }}
+                          value={input.value}
+                        />
+                        {meta.error && meta.touched && (
+                          <span>{meta.error}</span>
+                        )}
+                      </>
+                    )}
+                  />
+                </FormControl>
+              )}
+              <FormControl fullWidth className={classes.formControl}>
+                <InputLabel htmlFor="email">Email</InputLabel>
                 <Field
-                  name="username"
+                  name="email"
                   render={({ input, meta }) => (
-                    <Input
-                      id="username"
-                      type="text"
-                      inputProps={{
-                        ...input,
-                        autoComplete: "off"
-                      }}
-                      value={input.value}
-                    />
+                    <>
+                      <Input
+                        {...input}
+                        id="email"
+                        type="text"
+                        inputProps={{
+                          autoComplete: "off"
+                        }}
+                        value={input.value}
+                      />
+                      {meta.error && meta.touched && <span>{meta.error}</span>}
+                    </>
                   )}
                 />
               </FormControl>
-            )}
-            <FormControl fullWidth className={classes.formControl}>
-              <InputLabel htmlFor="email">Email</InputLabel>
-              <Field
-                name="email"
-                render={({ input, meta }) => (
-                  <Input
-                    id="email"
-                    type="text"
-                    inputProps={{
-                      ...input,
-                      autoComplete: "off"
-                    }}
-                    value={input.value}
-                  />
-                )}
-              />
-            </FormControl>
-            <FormControl fullWidth className={classes.formControl}>
-              <InputLabel htmlFor="password">Password</InputLabel>
-              <Field
-                type="password"
-                name="password"
-                render={({ input, meta }) => (
-                  <Input
-                    id="password"
-                    inputProps={{
-                      ...input,
-                      autoComplete: "off"
-                    }}
-                    value={input.value}
-                  />
-                )}
-              />
-            </FormControl>
-            <FormControl className={classes.formControl}>
-              <Grid
-                container
-                direction="row"
-                justify="space-between"
-                alignItems="center"
-              >
-                <Button
-                  type="submit"
-                  className={classes.formButton}
-                  variant="contained"
-                  size="large"
-                  color="secondary"
-                  disabled={submitting || pristine || invalid}
+              <FormControl fullWidth className={classes.formControl}>
+                <InputLabel htmlFor="password">Password</InputLabel>
+                <Field
+                  type="password"
+                  name="password"
+                  render={({ input, meta }) => (
+                    <>
+                      <Input
+                        {...input}
+                        id="password"
+                        inputProps={{
+                          autoComplete: "off"
+                        }}
+                        value={input.value}
+                      />
+                      {meta.error && meta.touched && <span>{meta.error}</span>}
+                    </>
+                  )}
+                />
+              </FormControl>
+              <FormControl className={classes.formControl}>
+                <Grid
+                  container
+                  direction="row"
+                  justify="space-between"
+                  alignItems="center"
                 >
-                  {this.state.formToggle ? "Enter" : "Create Account"}
-                </Button>
-                <Typography>
-                  <button
-                    className={classes.formToggle}
-                    type="button"
-                    onClick={() => {
-                      form.reset();
-                      this.setState({
-                        formToggle: !this.state.formToggle
-                      });
-                    }}
+                  <Button
+                    type="submit"
+                    className={classes.formButton}
+                    variant="contained"
+                    size="large"
+                    color="secondary"
+                    disabled={submitting || pristine || invalid}
                   >
-                    {this.state.formToggle
-                      ? "Create an account."
-                      : "Login to existing account."}
-                  </button>
+                    {this.state.formToggle ? "Enter" : "Create Account"}
+                  </Button>
+                  <Typography>
+                    <button
+                      className={classes.formToggle}
+                      type="button"
+                      onClick={() => {
+                        form.reset();
+                        this.setState({
+                          formToggle: !this.state.formToggle
+                        });
+                      }}
+                    >
+                      {this.state.formToggle
+                        ? "Create an account."
+                        : "Login to existing account."}
+                    </button>
+                  </Typography>
+                </Grid>
+              </FormControl>
+              {submitError && (
+                <Typography className={classes.errorMessage}>
+                  {submitError}
                 </Typography>
-              </Grid>
-            </FormControl>
-            {hasSubmitErrors && (
-              <Typography className={classes.errorMessage}>
-                {submitError}
-              </Typography>
-            )}
-          </form>
-        )}
+              )}
+            </form>
+          );
+        }}
       />
     );
   }

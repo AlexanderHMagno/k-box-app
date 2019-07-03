861b75fd2af19cbcdbd0f8aada636981da257d55
Yen Yin Hing <yenyinf287@gmail.com>
2019-07-02 20:27:52 -0700

    fix conditional on username

--- imports/ui/components/helpers/validation.js
+++ imports/ui/components/helpers/validation.js
@@ -1,4 +1,5 @@
 export default function validate(values) {
+  console.log(values, "nbooya");
   const errors = {};
 
   const regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -11,10 +12,11 @@ export default function validate(values) {
 
   if (!values.password) {
     errors.password = "Required";
-  } else if (!values.password.length < 5) {
+  } else if (values.password.length < 5) {
     errors.password = "Password must have at least 5 characters";
   }
-  if (!values.username) {
+
+  if (!values.username && document.getElementById("username") !== null) {
     errors.username = "Username is required";
   }
   return errors;

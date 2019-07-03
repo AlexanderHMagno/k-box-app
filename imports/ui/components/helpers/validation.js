1c1fa75a29d53fa76fdb85a2d72f97624fbae922
Yen Yin Hing <yenyinf287@gmail.com>
2019-07-02 18:52:35 -0700

    refactor conditional statements

--- imports/ui/components/helpers/validation.js
+++ imports/ui/components/helpers/validation.js
@@ -1,18 +1,21 @@
 export default function validate(values) {
   const errors = {};
-  let regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  if (!regex.test(values.email)) {
-    errors.email = "Enter a valid email address";
-  }
+
+  const regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
   if (!values.email) {
     errors.email = "required";
+  } else if (!regex.test(values.email)) {
+    errors.email = "Enter a valid email address";
   }
+
   if (!values.password) {
     errors.password = "Required";
+  } else if (!values.password.length < 5) {
+    errors.password = "Password must have at least 5 characters";
   }
-  if (values.username && !values.username) {
-    errors.username = "Required";
+  if (!values.username) {
+    errors.username = "Username is required";
   }
   return errors;
 }
